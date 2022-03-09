e467831de584406641da788fec47a7d5cd96fc85
Felipe <felipebalves10@hotmail.com>
2022-03-09 16:51:07 -0300

    Instânciado alguns objetos, criado layout.

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekBarGorjeta;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.editValor);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textTotal = findViewById(R.id.textTotal);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+    }
+
+}
